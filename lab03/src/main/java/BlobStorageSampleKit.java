bf583851e5c19e378e933406eb5030b6ba194f83
Katerina Andreadou <katerina.andreadou@prodyna.com>
2022-02-16 13:35:06 +0200

    Create new container and upload blob

--- lab03/src/main/java/BlobStorageSampleKit.java
+++ lab03/src/main/java/BlobStorageSampleKit.java
@@ -1,19 +1,28 @@
-import com.azure.storage.blob.BlobServiceClient;
-import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.*;
 import com.azure.storage.blob.models.StorageAccountInfo;
 import com.azure.storage.common.StorageSharedKeyCredential;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class BlobStorageSampleKit {
 
     static String BLOB_SERVICE = "<YOUR_BLOB_SERVICE_ENDPOINT>";
     static String STORAGE_ACCOUNT_NAME = "<YOUR_STORAGE_ACCOUNT_NAME>";
     static String ACCOUNT_KEY = "<ONE_OF_THE_STORAGE_ACCOUNT_KEYS>";
+    static String DOG_CONTAINER_NAME = "dogs";
+    static String DOG_FILE = "/dog.jpeg";
 
     public static void main(String args[]) {
-        listContainers();
+        try {
+            new BlobStorageSampleKit().listContainersAndBlobs();
+        } catch (URISyntaxException e) {
+            System.out.println(e);
+        }
     }
 
-    public static void listContainers() {
+    public void listContainersAndBlobs() throws URISyntaxException {
         StorageSharedKeyCredential credential = new StorageSharedKeyCredential(STORAGE_ACCOUNT_NAME, ACCOUNT_KEY);
         /* Create a new BlobServiceClient with a shared key credential */
         BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
@@ -28,7 +37,16 @@ public class BlobStorageSampleKit {
         /* List all containers in storage account */
         blobServiceClient.listBlobContainers().forEach(container -> {
             System.out.println("Container name: " + container.getName());
+            /* List all blobs in container */
+            BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(container.getName());
+            blobContainerClient.listBlobs().forEach(blobItem -> {
+                System.out.println("Blob name: " + blobItem.getName());
+            });
         });
-
+        /* Create new container and upload a blob */
+        BlobContainerClient dogContainer = blobServiceClient.createBlobContainer(DOG_CONTAINER_NAME);
+        BlobClient blobClient = dogContainer.getBlobClient(DOG_FILE);
+        URL resource = getClass().getClassLoader().getResource(DOG_FILE);
+        blobClient.uploadFromFile(Paths.get(resource.toURI()).toString());
     }
 }
